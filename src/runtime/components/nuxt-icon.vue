dbf698ae6614702e3242a94ac254bd16eba9b625
MohammadAli <aliyazdanim2@gmail.com>
2023-11-04 14:46:05 +0330

    refactor: remove some default svg styles

--- src/runtime/components/nuxt-icon.vue
+++ src/runtime/components/nuxt-icon.vue
@@ -1,49 +1,53 @@
 <template>
   <span
     class="nuxt-icon"
-    :class="{ 'nuxt-icon--fill': !filled, 'nuxt-icon--stroke': hasStroke && !filled }"
+    :class="{
+      'nuxt-icon--fill': !filled,
+      'nuxt-icon--stroke': hasStroke && !filled,
+    }"
     v-html="icon"
   />
 </template>
 
 <script setup lang="ts">
-import { ref, watchEffect } from '#imports'
+import { ref, watchEffect } from "#imports";
 
-const props = withDefaults(defineProps<{
-  name: string;
-  filled?: boolean
-}>(), { filled: false })
+const props = withDefaults(
+  defineProps<{
+    name: string;
+    filled?: boolean;
+  }>(),
+  { filled: false }
+);
 
+const icon = ref<string | Record<string, any>>("");
+let hasStroke = false;
 
-const icon = ref<string | Record<string, any>>('')
-let hasStroke = false
-
-async function getIcon () {
+async function getIcon() {
   try {
-    const iconsImport = import.meta.glob('assets/icons/**/**.svg', {
-      as: 'raw',
-      eager: false
-    })
-    const rawIcon = await iconsImport[`/assets/icons/${props.name}.svg`]()
-    if (rawIcon.includes('stroke')) { hasStroke = true }
-    icon.value = rawIcon
+    const iconsImport = import.meta.glob("assets/icons/**/**.svg", {
+      as: "raw",
+      eager: false,
+    });
+    const rawIcon = await iconsImport[`/assets/icons/${props.name}.svg`]();
+    if (rawIcon.includes("stroke")) {
+      hasStroke = true;
+    }
+    icon.value = rawIcon;
   } catch {
     console.error(
       `[nuxt-icons] Icon '${props.name}' doesn't exist in 'assets/icons'`
-    )
+    );
   }
 }
 
-await getIcon()
+await getIcon();
 
-watchEffect(getIcon)
+watchEffect(getIcon);
 </script>
 
 <style>
 .nuxt-icon svg {
-  width: 1em;
-  height: 1em;
-  margin-bottom: 0.125em;
   vertical-align: middle;
 }
 .nuxt-icon.nuxt-icon--fill,
@@ -52,7 +56,7 @@ watchEffect(getIcon)
 }
 
 .nuxt-icon.nuxt-icon--stroke,
-.nuxt-icon.nuxt-icon--stroke *{
+.nuxt-icon.nuxt-icon--stroke * {
   stroke: currentColor !important;
 }
 </style>
